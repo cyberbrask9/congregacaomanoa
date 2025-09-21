c1132a3999351f19dbdc99a12694ffa70a2b9377
manoa <cyberbrask3@gmail.comm>
2025-09-21 08:01:39 -0300

    Inicio da implementação do Menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Sistema de Gerenciamento de Objetos',
-  description: 'Sistema para gerenciar objetos com atribuições e privilégios',
+  title: 'Congregação Manoa',
+  description: 'Toda Groria a Jeová! ',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import FormularioObjeto from './components/FormularioObjeto';
 import ListaObjetos from './components/ListaObjetos';
+import MenuTopBar from './components/TopBar';
+import MenuGaveta from './components/MenuGaveta';
 import { Objeto } from '@/types';
 
 export default function Home() {
@@ -16,16 +18,14 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100">
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 py-6">
-          <h1 className="text-3xl font-bold text-gray-900 text-center">
-            Sistema de Gerenciamento de Objetos
-          </h1>
+          <MenuGaveta />
         </div>
       </header>
       
       <main className="max-w-7xl mx-auto py-8 px-4">
-        <FormularioObjeto onObjetoCriado={handleObjetoCriado} />
+       {/*  <FormularioObjeto onObjetoCriado={handleObjetoCriado} /> */}
         <div className="mt-12">
-          <ListaObjetos />
+          {/* <ListaObjetos /> */}
         </div>
       </main>
     </div>
